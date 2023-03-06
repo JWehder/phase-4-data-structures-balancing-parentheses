94646539f095c8736582c276feebdd52242add94
Jake Wehder <jw@email.comjake>
2023-03-06 13:12:02 -0600

    commit tree 43c50a12a129a6baef16724b9fc1b174ffaa10bf

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,22 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(string)
+    stack = Stack.new
+    
+    string.chars.each do |char|
+        if char == "("
+            stack.push(char)
+        elsif char == ")" && stack.peek == "("
+            stack.pop
+        else
+            stack.push(char)
+        end
+    end
+
+    stack.size
+    # use size to determine the number of broken parentheses
+   # use the last method to check the last parentheses shape
+   # needs an iterator to sort through each character
+   # the return value must be a number, so need a count variable
+end
